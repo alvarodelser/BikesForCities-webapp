b4d40fd0947c7c9a1e20603d62835d0ec2d23cbd
AlvarodelSer <a.fontecha.ser@gmail.com>
2026-03-27 12:23:13 +0100

    Routed about and compare pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import CityPage from "./pages/CityPage";
 import NotFoundPage from "./pages/NotFoundPage";
-// import ComparePage from "./pages/ComparePage";
-// import AboutPage from "./pages/AboutPage";
+import ComparePage from "./pages/ComparePage";
+import AboutPage from "./pages/AboutPage";
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -17,8 +17,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/map/:cityName" element={<CityPage />} />
         <Route path="*" element={<NotFoundPage />} />
-        {/* <Route path="/compare" element={<ComparePage />} /> */}
-        {/* <Route path="/about" element={<AboutPage />} /> */}
+        <Route path="/compare" element={<ComparePage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
       <Footer />
     </>
