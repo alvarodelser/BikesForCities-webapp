dad4aba72726fcedfee2ca3bb54675c8ff135af3
AlvarodelSer <a.fontecha.ser@gmail.com>
2026-06-11 11:36:58 +0200

    Added electoral semicircle and ingestion for economic data

--- backend/database/migrations/021_budget_categories_classification.sql
+++ backend/database/migrations/021_budget_categories_classification.sql
@@ -2,7 +2,7 @@ ALTER TABLE city_budget_categories
     ADD COLUMN IF NOT EXISTS classification VARCHAR(16) NOT NULL DEFAULT 'functional';
 
 ALTER TABLE city_budget_categories
-    DROP CONSTRAINT IF EXISTS city_budget_categories_city_id_year_budget_type_category_code_key;
+    DROP CONSTRAINT IF EXISTS city_budget_categories_city_id_year_budget_type_category_co_key;
 
 ALTER TABLE city_budget_categories
     ADD CONSTRAINT city_budget_categories_unique
---
+++ backend/database/migrations/022_fix_budget_categories_constraint.sql
@@ -0,0 +1,2 @@
+ALTER TABLE city_budget_categories
+    DROP CONSTRAINT IF EXISTS city_budget_categories_city_id_year_budget_type_category_co_key;
